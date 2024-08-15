ce9106e2163c661ee6850d856bb779c6e17758b4
shahTanya <shahtania19@gmail.com>
2024-08-15 23:51:19 +0000

    README.md

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,22 @@
-#!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+#!/usr/bin/python2
+
+"""Queries Reddit API and returns the number of subscribers (not active user,
+                                                             total subscribers)
+for a given subreddit
+
+Returns:
+    0 if subreddit is invalid
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Queries reddit api for subscribers number in a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "shahTanya/v0.0.1"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json()["data"]
+        return (data["subscribers"])
+    else: 
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
