a4a29b078a9e90d7512a0c1212983151ded2c360
shahTanya <shahtania19@gmail.com>
2024-08-16 00:35:14 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Get the top 10 post in a reddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """Gets the 10 top post"""
+    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+    headers = {"User-Agent": "chukssomzzy-app/v0.0.1"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        top_ten_posts = r.json()["data"]
+        for top_post in top_ten_posts["children"]:
+            print(top_post["data"]["title"])
+    else:
+        print(None
